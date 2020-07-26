11108c0bea068d48fb507137ee57c43c312158d8
mprunet <mprunet@safepic.fr>
2020-07-26 21:17:39 +0200

    XPathFix

---
+++ src/org/owasp/psafix/top10/vulnerable/A1XPathFix.java
@@ -0,0 +1,49 @@
+package org.owasp.psafix.top10.vulnerable;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class A1XPathFix {
+    private boolean doLogin(String userName, char[] password)
+            throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        domFactory.setNamespaceAware(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder();
+        Document doc = builder.parse("users.xml");
+        String pwd = hashPassword( password);
+
+        if (!userName.matches("[\\w]*")) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        XPathFactory factory = XPathFactory.newInstance();
+        XPath xpath = factory.newXPath();
+        XPathExpression expr = xpath.compile("//users/user[username/text()='" +
+                userName + "' and password/text()='" + pwd + "' ]");
+        Object result = expr.evaluate(doc, XPathConstants.NODESET);
+        NodeList nodes = (NodeList) result;
+
+        // Print first names to the console
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i).getChildNodes().item(1).getChildNodes().item(0);
+            System.out.println( "Authenticated: " + node.getNodeValue());
+        }
+
+        return (nodes.getLength() >= 1);
+    }
+
+    private String hashPassword(char[] password) {
+        return "";
+    }
+}
